beeae9b2c3253b86a63d019e8ecde36446e9b254
svasandani <vasandanishailesh@gmail.com>
2020-07-29 15:00:17 -0400

    Add basic first round implementation

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,8 @@ fn main() -> io::Result<()> {
 
     reader.read_line(&mut throwaway)?;
 
-    let mut group_a_preferences: Vec<i32> = Vec::with_capacity(num_people * num_people);
+    // store prefs in a 2d Vec
+    let mut group_a_preferences: Vec<usize> = Vec::with_capacity(num_people * num_people);
 
     let mut a = 0;
     // for each person in group A
@@ -52,7 +53,8 @@ fn main() -> io::Result<()> {
 
     reader.read_line(&mut throwaway)?;
 
-    let mut group_b_preferences: Vec<i32> = Vec::with_capacity(num_people * num_people);
+    // store prefs in a 2d Vec
+    let mut group_b_preferences: Vec<usize> = Vec::with_capacity(num_people * num_people);
 
     let mut b = 0;
     // for each person in group B
@@ -78,6 +80,68 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-fn stable_match(num_people: usize, group_a: Vec<i32>, group_b: Vec<i32>) -> Vec<i32> {
-    Vec::with_capacity(num_people * num_people)
+fn stable_match(num_people: usize, group_a_preferences: Vec<usize>, group_b_preferences: Vec<usize>) -> Vec<usize> {
+    let max = usize::MAX;
+
+    // store group_a's provisional engagements (we eventually return this)
+    let mut provisional_matches: Vec<usize> = vec![max; num_people];
+
+    // store group_b's offers in a similar array
+    let mut group_b_offers: Vec<usize> = vec![max; num_people];
+
+    // first round
+    // each member of group a proposes a match to its most preferred
+    let mut a = 0;
+    // for each person in group A
+    while a < num_people {
+        // get the highest ranking preference
+        // and offer them a match
+        let a_best_match = group_a_preferences[a * num_people];
+        let b_current_best_offer = group_b_offers[a_best_match];
+
+        // if the person in group B has had no offer
+        if b_current_best_offer == max {
+            // we provisionally match the two
+            provisional_matches[a_best_match] = a;
+        } else {
+            // otherwise
+            // we see if A is better than B's current best offer
+            let better: bool = false;
+            let mut i = a_best_match * num_people;
+
+            // for each of B's preferences
+            while i <  (a_best_match + 1) * num_people {
+                // if we reach A first, then A is better
+                if group_b_preferences[i] == a {
+                    better = true;
+                    break;
+                } else if group_b_preferences[i] == group_b_offers[a_best_match] {
+                    // but if we reach the current match, then A is not better
+                    better = false;
+                    break;
+                }
+            }
+
+            // if A is better 
+            if better {
+                // unmatch the previous best_match
+                provisional_matches[group_b_offers[a_best_match]] = max;
+                // match the current best_match
+                provisional_matches[a] = a_best_match;
+                group_b_offers[a_best_match] = a;
+            }
+            // otherwise do nothing
+            
+        }
+
+        a = a + 1;
+    }
+
+    while (provisional_matches.contains(&max)) {
+        println!("hello");
+        break;
+    }
+
+    // once there are no more -1 values, we have our stable match
+    return provisional_matches;
 }
